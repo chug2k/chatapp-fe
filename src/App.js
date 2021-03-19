7ebc62c61b9772d9033e51acf357d97242937154
Charles Lee <chug2k@Product-Market-Fit.local>
2021-03-19 10:41:02 +0700

    forgot to commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import socketIOClient from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+let socket;
 
 function App() {
+  useEffect(() => {
+    socket = socketIOClient("http://35c071c0a287.ngrok.io/");
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("receive", (msg) => {
+      setMessages([...messages, msg]);
+    });
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("Sending ... " + newMessage);
+    socket.emit("send", newMessage);
+    setNewMessage("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World! </h1>
+      <form onSubmit={sendMessage}>
+        <label htmlFor="message">Enter your message</label>
+        <input
+          type="text"
+          name="message"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <input type="submit" value="send message" />
+      </form>
+      <ul>
+        {messages.map((m) => (
+          <li>{m}</li>
+        ))}
+      </ul>
     </div>
   );
 }
